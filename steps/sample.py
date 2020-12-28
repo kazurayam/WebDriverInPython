5702d5840675d868f4f6c7795d949a62aa672186
kazurayam <kazuaki.urayama@gmail.com>
2020-12-28 13:16:20 +0900

    chromedriver_path_mac

--- steps/sample.py
+++ steps/sample.py
@@ -1,7 +1,9 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+chromedriver_path_mac = '/Applications/Katalon Studio.app/Contents/Eclipse/configuration/resources/drivers/chromedriver_mac/chromedriver'
+
+driver = webdriver.Chrome(executable_path=chromedriver_path_mac)
 driver.get('https://www.google.com/')
 time.sleep(5)
 search_box = driver.find_element_by_name("q")
